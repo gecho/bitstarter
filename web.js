8d6de048730ccfcb35cbeb324399deff62fbfc11
gecho <quixoticventure@gmail.com>
2013-07-23 12:30:06 +0000

    redeploy web.js

--- web.js
+++ web.js
@@ -4,13 +4,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFile('index.html', function(err, data){
-    if(err) throw err;
-});
+//var buffer = fs.readFile('index.html', function(err, data){
+//    if(err) throw err;
+//});
 
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+var buffer = fs.readFileSync('index.html');
+var out = buffer.toSring();  
+response.send();
 });
 
 var port = process.env.PORT || 8080;

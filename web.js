0667ef17456f60e480f4bcc88ef6235281e5569d
gecho <quixoticventure@gmail.com>
2013-07-23 12:24:48 +0000

    redeploy web.js

---
+++ web.js
@@ -0,0 +1,19 @@
+require ('fs');
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var buffer = fs.readFile('index.html', function(err, data){
+    if(err) throw err;
+});
+
+
+app.get('/', function(request, response) {
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});

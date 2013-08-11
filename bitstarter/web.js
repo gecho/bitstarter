e244a73cef07717ce3f35d685f04b2650a1ed191
Ubuntu <ubuntu@ip-172-31-41-16.us-west-2.compute.internal>
2013-08-11 06:17:06 +0000

    updated webjs

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World  2!');
+    var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
